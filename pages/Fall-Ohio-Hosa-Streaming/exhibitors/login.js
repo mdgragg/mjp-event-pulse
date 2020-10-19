e40fb13f0463d8098c632445e1080ca9b96d106a
Al Sigman <asigman@MJP0988.local>
2020-10-19 11:05:48 -0400

    added hosa streaming

---
+++ pages/Fall-Ohio-Hosa-Streaming/exhibitors/login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const login = (props) => {
+  return (
+    <div>
+      <h1> Log In</h1>
+    </div>
+  );
+};
+
+login.propTypes = {};
+
+export default login;
---
+++ pages/Fall-Ohio-Hosa-Streaming/style.js
@@ -0,0 +1,7 @@
+// This is the working version of theme colors you can change.abs
+//This will override the template colors
+
+export const theme = {
+  // bg: '#BADA55'
+  fontFamily: "Roboto",
+};
