4cd00d61da6206990417790309268521345ef562
NightingaleMedia.AlbertMacbook <alsigman@gmail.com>
2021-01-05 12:57:08 -0500

    added correcnt endpoint in login logic

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
 module.exports = {
   distDir: 'build',
-  env: {
-    NEXT_PUBLIC_STRAPI_API_URL: `${process.env.NEXT_PUBLIC_STRAPI_API_URL}`,
-    STRAPI_AUTHENTICATED_API_KEY: `${process.env.STRAPI_AUTHENTICATED_API_KEY}`,
-  },
 };
