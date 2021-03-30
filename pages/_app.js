5dcf18c1c8e98b2cb8fc090618fc0e6995d92127
NightingaleMedia.AlbertMacbook <alsigman@gmail.com>
2021-03-30 16:21:05 -0400

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import App, { Container } from 'next/app';
+import { useRouter } from 'next/router';
 import Meta from 'components/globals/Meta';
 import { GlobalStyle } from 'components/globals/GlobalStyle';
 import 'react-dropzone-uploader/dist/styles.css';
@@ -7,27 +8,37 @@ import './global.css';
 import cookies from 'next-cookies';
 import UserContextProvider from '../lib/context/UserContext';
 import AppContextProvider from '../lib/context/AppContext';
-
+import * as gtag from '../lib/analytics';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
-    return (
-      <>
-        <Meta />
-        <GlobalStyle />
-        <AppContextProvider>
-          <UserContextProvider loginData={this.props.loginData}>
-            <ToastContainer />
-            <Component {...pageProps} />
-          </UserContextProvider>
-        </AppContextProvider>
-      </>
-    );
-  }
+function MyApp({ Component, pageProps, loginData }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageView(url);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+  return (
+    <>
+      <Meta />
+      <GlobalStyle />
+      <AppContextProvider>
+        <UserContextProvider loginData={loginData}>
+          <ToastContainer />
+          <Component {...pageProps} />
+        </UserContextProvider>
+      </AppContextProvider>
+    </>
+  );
 }
+
 // import withApollo from "next-with-apollo";
 // import { ApolloProvider } from "@apollo/react-hooks";
 // import ApolloClient, { InMemoryCache } from "apollo-boost";
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
-import React from "react";
-import Document, { Head, Main, NextScript, Html } from "next/document";
-import { ServerStyleSheet } from "styled-components";
+import React from 'react';
+import Document, { Head, Main, NextScript, Html } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
@@ -15,7 +15,22 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>{this.props.styleTags}</Head>
+        <Head>
+          {this.props.styleTags}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `  window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+`,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
