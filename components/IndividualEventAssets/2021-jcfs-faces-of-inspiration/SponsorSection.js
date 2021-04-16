e611a7a89eff781800448745d57deb603122650e
NightingaleMedia.AlbertMacbook <alsigman@gmail.com>
2021-04-16 17:57:31 -0400

    fixed up logos

--- components/IndividualEventAssets/2021-jcfs-faces-of-inspiration/SponsorSection.js
+++ components/IndividualEventAssets/2021-jcfs-faces-of-inspiration/SponsorSection.js
@@ -30,6 +30,9 @@ const SingleSponsor = styled.div`
   && img {
     height: 250px;
     width: auto;
+    padding: 20px;
+    background-color: white;
+    box-shadow: 0px 0px 12px -10px;
   }
   && h3 {
     color: ${(props) => props.theme.blue};
