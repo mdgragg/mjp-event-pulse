7cba5f86ad78640c3d65d05cc3d0390ed1abf68e
NightingaleMedia.AlbertMacbook <alsigman@gmail.com>
2021-05-05 11:47:21 -0400

    alliance data

---
+++ components/Sections/Section__WithBG.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const BGSection = styled.div`
+  /* background-color: red; */
+  position: relative;
+  background-color: rgba(0, 0, 0, 0);
+  &&::before {
+    content: '';
+    position: absolute;
+    background-image: url('${(props) => props.imgSrc}');
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    left: 0;
+  }
+`;
+
+const Section__WithBG = ({
+  children,
+  imgSrc = 'https://placehold.co/1920x1080',
+}) => {
+  return <BGSection imgSrc={imgSrc}> {children}</BGSection>;
+};
+
+export default Section__WithBG;
