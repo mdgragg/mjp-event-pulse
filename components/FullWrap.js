c64cc3d4b9fe86a88501ad093afb0eb78cf8248c
NightingaleMedia.AlbertMacbook <alsigman@gmail.com>
2021-05-14 16:43:29 -0400

    fixed blur

--- components/FullWrap.js
+++ components/FullWrap.js
@@ -5,12 +5,12 @@ import React from 'react';
 const Wrap = styled.div`
   filter: blur(0px);
   &&.blurred {
-    filter: blur(25px);
+    filter: blur(18px);
   }
 `;
 
-const FullWrap = ({ children }) => {
-  return <Wrap>{children}</Wrap>;
+const FullWrap = ({ children, className }) => {
+  return <Wrap className={className}>{children}</Wrap>;
 };
 
 export default FullWrap;
