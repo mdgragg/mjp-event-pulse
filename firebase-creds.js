1425a4583cb3f599340d372575e482b2c12f6d0b
NightingaleMedia.AlbertMacbook <alsigman@gmail.com>
2021-08-09 10:42:59 -0400

    added firebase creds

---
+++ firebase-creds.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: process.env.FB_API_KEY,
+  authDomain: 'millsjameseventcms-testing.firebaseapp.com',
+  databaseURL: process.env.FB_DATABASE_URL,
+  projectId: 'millsjameseventcms-testing',
+  storageBucket: 'millsjameseventcms-testing.appspot.com',
+  messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+  appId: process.env.FB_APP_ID,
+  measurementId: process.env.FB_MEASUREMENT_ID,
+};
