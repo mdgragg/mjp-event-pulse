b11163e455697d30f3489f8970b30827558c3fca
NightingaleMedia.AlbertMacbook <alsigman@gmail.com>
2021-09-29 16:42:30 -0400

    pizzuti

---
+++ eventAssets/legends-and-legacies-honoring-the-pizzutis/legends-and-legacies-honoring-the-pizzutis.theme.ts
@@ -0,0 +1,171 @@
+import { createTheme } from '@material-ui/core';
+import type { BREAKPOINTS, BUTTONS, COLORS, FONTS, GLOBALS, MJxTheme, PALETTE, TYPOGRAPHY } from 'types/Theme';
+
+const THEME_COLORS: COLORS = {
+  red: 'tomato',
+  orange: '#FF5600',
+  yellow: '#edad13',
+  green: '#97d700',
+  blue: '#1e2c60',
+  indigo: '#0d2f49',
+  violet: '#5602b7',
+  white: '#ffffff',
+  black: '#000000',
+  primary: 'white',
+  secondary: '#1e372f',
+  tertiary: '#4f4f4d',
+  info: '',
+  warning: '',
+  offWhite: '#f7f7f7',
+  offBlack: '#686b75',
+};
+const THEME_PALETTE: PALETTE = {
+  text: {
+    primary: THEME_COLORS.black,
+    secondary: THEME_COLORS.black,
+    tertiary: THEME_COLORS.white,
+    error: 'red',
+    info: 'blue',
+    success: 'green'
+  },
+  background: {
+    primary: THEME_COLORS.white,
+    secondary: THEME_COLORS.offWhite,
+    tertiary: THEME_COLORS.orange,
+    error: 'red',
+    info: 'blue',
+    success: 'green'
+  },
+  error: THEME_COLORS.red,
+  info: 'blue',
+  success: 'green'
+
+};
+const THEME_FONTS: FONTS = {
+  title: {
+    fontFamily: 'Roboto ',
+    fontSize: 'clamp(1.5rem, 20vw, 2rem)',
+    letterSpacing: '',
+    fontWeight: '800',
+    lineHeight: '3rem',
+
+  },
+  body: {
+    fontFamily: 'Roboto',
+    fontWeight: '200',
+    fontSize: 'clamp(1.25rem, 20vw, 1rem)',
+    letterSpacing: '',
+    lineHeight: 'auto',
+  },
+  get secondary() {
+    return this.body;
+  }, // default to body
+  get tertiary() {
+    return this.body;
+  }, // default to body
+};
+
+export const THEME_BUTTONS: BUTTONS = {
+  primary: {
+    fontFamily: 'Roboto',
+    fontSize: '0.85rem',
+    letterSpacing: '2px',
+    lineHeight: 'inherit',
+    backgroundColor: THEME_COLORS.orange,
+    color: 'white',
+    border: '',
+  },
+  secondary: {
+    fontFamily: THEME_FONTS.body.fontFamily,
+    fontSize: '1rem',
+    letterSpacing: '',
+    lineHeight: '',
+    backgroundColor: '',
+    color: '',
+    border: '',
+  },
+  big: {
+    fontFamily: 'Roboto',
+    fontSize: '2rem',
+    letterSpacing: '2px',
+    lineHeight: 'auto',
+    backgroundColor: THEME_COLORS.primary,
+    color: THEME_COLORS.secondary,
+    border: '5px solid white',
+  },
+  small: {
+    fontFamily: '',
+    fontSize: '',
+    letterSpacing: '',
+    lineHeight: '',
+    backgroundColor: '',
+    color: '',
+    border: '',
+  },
+  hover: {
+    color: 'white',
+    backgroundColor: THEME_COLORS.offBlack,
+  },
+};
+
+const THEME_BREAKPOINTS : BREAKPOINTS= {
+  xl: 1920,
+  lg: 1600,
+  md: 1200,
+  sm: 768,
+  xs: 500,
+};
+
+const THEME_TYPOGRAPHY: TYPOGRAPHY = {
+  h1: {
+    color: THEME_COLORS.primary,
+    backgroundColor: THEME_COLORS.yellow,
+    padding: '1rem',
+    ...THEME_FONTS.title,
+  },
+  h2: {
+    color: THEME_COLORS.white,
+    textAlign: 'center',
+    margin: '1rem',
+    backgroundColor: THEME_COLORS.yellow,
+  },
+  h3: {
+    color: THEME_COLORS.black,
+  },
+  h4: {
+    color: THEME_COLORS.primary,
+  },
+  h5: {
+    color: THEME_COLORS.primary,
+  },
+  h6: {
+    color: THEME_COLORS.primary,
+  },
+  body1: {
+    ...THEME_FONTS.body,
+  },
+  body2: {
+    ...THEME_FONTS.body,
+  },
+};
+
+const THEME_GLOBALS :GLOBALS = {
+  heroHeight: '550px',
+  heroBgColor: THEME_COLORS.black,
+  headerOpacity: '1',
+  videoBreakPoint: 700,
+  maxSectionWidth: '1800px',
+} 
+
+export const mjxTheme: MJxTheme = {
+  muiTheme:{ ...createTheme()},
+  typography: { ...THEME_TYPOGRAPHY },
+  colors: { ...THEME_COLORS },
+  palette: { ...THEME_PALETTE },
+  fonts: { ...THEME_FONTS },
+  buttons: { ...THEME_BUTTONS },
+  breakpoints: { ...THEME_BREAKPOINTS },
+  ...THEME_GLOBALS
+};
+
+export default mjxTheme;
---
+++ pages/legends-and-legacies-honoring-the-pizzutis/index.tsx
@@ -0,0 +1,152 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { getEventMeta } from 'lib/api';
+import useCalculateIfStarted from 'hooks/useCalculateIfStarted';
+import AuthWrap from 'components/AuthWrap';
+import Meta from 'components/__GLOBALS__/Meta';
+import ThemedPage from 'components/__GLOBALS__/ThemedPage';
+import Body from 'components/template1/Body';
+import { Banner__WithPicture } from 'components/Banners';
+import FlexHero from 'components/Heroes/FlexHero';
+import DateParse from 'components/__Assets__/DateParse';
+import Counter__JustNumbers from 'components/Counters/Counter__JustNumbers';
+import { CenteredPlayer, PlayerWithChat } from 'components/BodyTemplates';
+import { toast } from 'react-toastify';
+import Center from 'components/Center';
+import default_theme from 'eventAssets/legends-and-legacies-honoring-the-pizzutis/legends-and-legacies-honoring-the-pizzutis.theme';
+import { GET_SERVERSIDE_PROPS_DEFAULT } from 'src/page_responses/default';
+import BodyWrap from 'components/BodyTemplates/BodyWrap';
+import { StaticResponse } from 'types/PageResponses';
+import { GetStaticProps } from 'next';
+import { BoxedCounter } from 'components/Counters';
+
+const EVENT_URL = `legends-and-legacies-honoring-the-pizzutis`;
+const PLACEHOLD = 'https://placehold.co/';
+
+const Index = (props) => {
+  const router = useRouter();
+  const EVENT_URL = router.query.event;
+  const { event_meta, main_event } = props;
+
+  var event_theme = {
+    ...default_theme,
+    header_image: main_event?.HeaderImage?.url || PLACEHOLD + '1920x1080',
+  };
+
+  const hasStarted = useCalculateIfStarted(main_event);
+  const [auth, setAuth] = useState(false);
+
+  return (
+    <ThemedPage theme={event_theme}>
+      <AuthWrap
+        eventToCheck={main_event}
+        successCallback={(res) => {
+          toast.success(
+            `Hello ${
+              res.Attendee.AttendeeFirst ? res.Attendee.AttendeeFirst : ''
+            }, welcome to ${main_event.EventName}`
+          );
+        }}
+      >
+        <Meta title={main_event.EventName}>
+          <title>{main_event.EventName}</title>
+        </Meta>
+        <FlexHero>
+          <div>
+            <img
+              style={{
+                width: '100%',
+                maxWidth: '350px',
+                margin: '2rem auto',
+              }}
+              src={main_event.LogoLink[1]?.Media?.url || null}
+            />
+          </div>
+          <div>
+            <Center>
+              <h1 style={{ margin: 'auto' }}>{main_event.EventName}</h1>
+              <h2 style={{ margin: '2rem auto' }}>
+                <i>
+                  <DateParse date={main_event.eventStartEnd.StartDateTime} />
+                </i>
+              </h2>
+            </Center>
+          </div>
+          <div>
+            <Center>
+              <BoxedCounter event={main_event}></BoxedCounter>
+            </Center>
+          </div>
+        </FlexHero>
+        <BodyWrap>
+          {main_event.streamLinks.length === 1 ? (
+            <div
+              style={{
+                minHeight: '60vh',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+              }}
+            >
+              <CenteredPlayer
+                showing={true}
+                hasStarted={true}
+                videoUrl={main_event.streamLinks[0].url}
+              />
+            </div>
+          ) : (
+            <div
+              style={{
+                minHeight: '60vh',
+                backgroundColor: 'none',
+                margin: '2rem',
+              }}
+            >
+              <PlayerWithChat
+                children={null}
+                hasStarted={true}
+                videoUrl={main_event.streamLinks[0].url}
+                chatUrl={main_event.streamLinks[1].url}
+              />
+            </div>
+          )}
+
+          {main_event.Description && (
+            <Banner__WithPicture
+              imgUrl={main_event.LogoLink[0]?.Media?.url || null}
+              color={'black'}
+              secondary={`white`}
+              headerText={`About This Event`}
+              innerWidth={`650px`}
+              buttonText={`Learn More`}
+              buttonLink={main_event.LogoLink[0]?.Link || '#'}
+            >
+              {main_event.Description}
+            </Banner__WithPicture>
+          )}
+        </BodyWrap>
+        <img
+          src={main_event.LogoLink[0]?.Media?.url}
+          style={{ width: '100%', height: 'auto' }}
+        />
+      </AuthWrap>
+    </ThemedPage>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  let event_data = await getEventMeta(EVENT_URL);
+  let main_event = event_data.events.filter((ev) => ev.isMainEvent === true)[0];
+
+  const returnObj: StaticResponse = {
+    props: {
+      event_meta: event_data,
+      main_event,
+    },
+    revalidate: 300,
+  };
+
+  return returnObj;
+};
+
+export default Index;
