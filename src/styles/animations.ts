3e6f323733fad7eab4bcb4cf3de9f5f52a88fb00
NightingaleMedia.AlbertMacbook <alsigman@gmail.com>
2021-10-18 11:54:30 -0400

    added activists

--- src/styles/animations.ts
+++ src/styles/animations.ts
@@ -1,3 +1,12 @@
-export const test = () => {
-    return "test"
+import { keyframes } from 'styled-components'
+export const bob = keyframes`
+0%{
+  transform: scale(1)
 }
+50%{
+  transform: scale(1.2)
+}
+100%{
+  transform: scale(1)
+}
+`;
--- types/Events__Types.ts
+++ types/Events__Types.ts
@@ -1,18 +1,31 @@
 export type EventStartEnd__Type = {
-  StartDateTime: string;
-  EndDateTime: string;
+  StartDateTime: Date;
+  EndDateTime:  Date ;
 };
 
+export interface Event_Job {
+  id: number;
+  jobId: string | number;
+  client: any;
+  EventJobName: string;
+  DriveFolder: string;
+}
 export interface Event__Type {
   id: string | number;
+  EventName: string;
+isMainEvent: boolean;
+client: any;
+slug: string;
+created_at: Date;
+updated_at: Date;
+Description?: null | string;
+
   event_job: {
     jobId: number;
   };
-  EventName: string;
   AuthOptions?: {
     AuthorizationType: string;
   };
   BreakoutSessions?: [any];
-  Description?: null | string;
   eventStartEnd: EventStartEnd__Type;
 }
