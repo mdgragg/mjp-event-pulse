db3677d89a1d8fe1f954f541da08eaa3f810985e
NightingaleMedia.AlbertMacbook <alsigman@gmail.com>
2021-10-21 09:19:09 -0400

    added link

--- components/AuthWrap/Modals/AuthModal__Register.tsx
+++ components/AuthWrap/Modals/AuthModal__Register.tsx
@@ -108,7 +108,6 @@ export default function AuthModal__Register(props: AuthModalRegisterProps) {
       {
         ...send_values,
         ...emailOptions,
-        url: `https://mjvirtualevents.com/worthingtoninvestorday`,
       },
       eventToCheck.id
     )
