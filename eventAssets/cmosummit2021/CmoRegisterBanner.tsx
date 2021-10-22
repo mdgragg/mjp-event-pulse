03966e481f350660c73bd3ebd8978c9d4db0f0fa
NightingaleMedia.AlbertMacbook <alsigman@gmail.com>
2021-10-22 12:25:04 -0400

    added cmo register

--- eventAssets/cmosummit2021/CmoRegisterBanner.tsx
+++ eventAssets/cmosummit2021/CmoRegisterBanner.tsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import { AuthModal__AttendeeListRegister } from 'components/AuthWrap/Modals'
+import { Button__Primary } from 'components/Buttons'
+import React, { useState } from 'react'
+import { toast } from 'react-toastify'
 import styled from 'styled-components'
 
 const StyledBanner = styled.div`
   background-color: blue;
   min-height: 450px;
 `
-const CmoRegisterBanner = () => {
-  return <StyledBanner>REGISTER</StyledBanner>
+const CmoRegisterBanner = ({ main_event, theme }) => {
+  const [registerOpen, setRegisterOpen] = useState(false)
+  return (
+    <StyledBanner>
+      <Button__Primary>Register</Button__Primary>
+      <AuthModal__AttendeeListRegister
+        eventToCheck={main_event}
+        successCallback={() => toast.success('Success')}
+        theme={theme}
+        open={registerOpen}
+      />
+    </StyledBanner>
+  )
 }
 
 export default CmoRegisterBanner
